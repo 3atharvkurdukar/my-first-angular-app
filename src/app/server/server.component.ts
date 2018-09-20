bc3c62f4193e42c178331600ad08eca1d5b6a972
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-09-20 22:41:50 +0530

    Using inline template instead of HTML file

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    template: '<h4>The ServerComponent</h4>'
 })
 export class ServerComponent {
 
