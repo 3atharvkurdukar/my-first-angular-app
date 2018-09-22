bb3ba855f16670c34eda1f70b0c71d2988c31471
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-09-22 22:45:41 +0530

    Stying elements dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,7 +10,16 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverID = 10;
     serverStatus = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'lightgreen' : 'red';
     }
 }
