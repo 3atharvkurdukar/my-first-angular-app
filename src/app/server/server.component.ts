b12330d38e3bd63342c950bb9c770e994b2316ae
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2018-09-22 23:05:58 +0530

    Applying classes dynamically using ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,11 +1,16 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 /**
  * The component can also be created using CLI as
  * 'ng generate component component_name' or 'ng g c component_name'
  */
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+      .offline {
+        color: white
+      }
+    `]
 })
 export class ServerComponent {
     serverID = 10;
